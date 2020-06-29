0343cefe5b61b99c09d3f33b0c2e17e94d5d1e96
acrylic-style <me@acrylicstyle.xyz>
2020-06-29 17:48:58 +0900

    add perm

--- src/main/java/xyz/acrylicstyle/simplecommands/commands/SpawnCommand.java
+++ src/main/java/xyz/acrylicstyle/simplecommands/commands/SpawnCommand.java
@@ -7,7 +7,7 @@ import xyz.acrylicstyle.tomeito_api.command.PlayerCommandExecutor;
 public class SpawnCommand extends PlayerCommandExecutor {
     @Override
     public void onCommand(Player player, String[] args) {
-        if (!player.getWorld().getName().equalsIgnoreCase("world")) {
+        if (!player.hasPermission("simplecommands.spawn") && !player.getWorld().getName().equalsIgnoreCase("world")) {
             player.sendMessage(ChatColor.RED + "このワールドで使用できません。");
             return;
         }
